c0dc8f727aa59647da4307f8edc9d33469af4388
Erik Hermansen <info@seespacelabs.com>
2024-08-11 11:16:29 -0700

    Added floating point precision to slider values. Added a handler for getting updated values while dragging.

--- src/components/slider/SliderGalleryItem.tsx
+++ src/components/slider/SliderGalleryItem.tsx
@@ -1,10 +1,17 @@
 import Slider from "./Slider";
 import GalleryItem from "@/components/galleryItem/GalleryItem";
 
+import { useState } from "react";
+
 function SliderGalleryItem() {
+  const [updatedValue, setUpdatedValue] = useState(50);
+  const [finalValue, setFinalValue] = useState(50);
+
   return (
     <GalleryItem title="Slider" description="A slider component">
-      <Slider value={50} onChange={() => {}}/>
+      <p>Updated value: {updatedValue}</p>
+      <p>Final value: {finalValue}</p>
+      <Slider value={finalValue} onChange={setFinalValue} onUpdate={setUpdatedValue}/>
     </GalleryItem>
   );
 }
